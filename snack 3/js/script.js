296e82c3cd2e13340357ed3438aec899e3069242
DanieleBegnis <daniele.begnis@gmail.com>
2024-03-11 16:37:52 +0100

    add for and console leg

--- snack 3/js/script.js
+++ snack 3/js/script.js
@@ -1,1 +1,7 @@
 //Crea un array vuoto. Chiedi per 6 volte all’utente di inserire un numero, se è dispari inseriscilo nell’array.
+const numbers =[];
+for(let i = 0; i < 6; i++) {
+    //chiedo all'utente di inserire sei numeri
+    const userNumbers = parseInt(prompt('dammi un numero'))
+    console.log(userNumbers)
+}
