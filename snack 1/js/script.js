95847b9e9b878b33ddda479ce5d3fdae00377128
DanieleBegnis <daniele.begnis@gmail.com>
2024-03-11 16:28:55 +0100

    feat: add prompt and push

--- snack 1/js/script.js
+++ snack 1/js/script.js
@@ -1,3 +1,13 @@
 //Chiedere all'utente 5 numeri. Inserire i numeri nell'array ma senza creare duplicati (inserire il numero nell'array solo se non è già presente).
 const numbers =[];
 console.log(numbers)
+for(let i = 0; i < 5; i++) {
+    //chiedere all'utente un numero
+    const userNumbers = parseInt(prompt('dimmi un numero'));
+    
+    
+    if(numbers.includes(userNumbers) === false) {
+        numbers.push(userNumbers);
+    }
+
+}
